c45bd1a46c6bdaef74e67fad3a5a21b6a2e029cd
DESKTOP-DUCNEIH\HokageYeah <659672626@qq.com>
2023-07-01 00:04:08 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,5 @@ program.command("install").action(async (version) => {
 
 console.log(process.argv);
 console.log(chalk.red('红色文字'));
-// 解析命令行参数 window 11111
+// 解析命令行参数 window
 program.parse(process.argv);
