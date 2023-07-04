a2ff2d9ce1a389c80ce3ca53a1090daf72c6a4ca
yuye <18838280615@163.com>
2023-07-04 18:24:45 +0800

    提交npm

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,17 @@ import { useMirror } from "./code/useMirror";
 // rednpm - http://registry.mirror.cqupt.edu.cn/
 // skimdb - https://skimdb.npmjs.com/registry/
 
-program.command("install").action(async (version) => {
-  const a: string = "1";
-  const url = import.meta.url;
-  //   console.log("__dirname", __dirname);
-  console.log("url", url);
-  // console.log(url);
-  console.log("到合适的黄金时代手机壳", a);
-});
+// program.command("install").action(async (version) => {
+//   const a: string = "1";
+//   const url = import.meta.url;
+//   //   console.log("__dirname", __dirname);
+//   console.log("url", url);
+//   // console.log(url);
+//   console.log("到合适的黄金时代手机壳", a);
+// });
 
-console.log(process.argv);
-console.log(chalk.red("红色文字"));
+// console.log(process.argv);
+// console.log(chalk.red("红色文字"));
 
 // 查看所有镜像
 commandFn("ls", "查看所有镜像", () => {
